01c86d454c68ddbf9945fd3c4d319260a906ce65
zumaku <60200121101@uin-alauddin.ac.id>
2023-01-26 10:18:50 +0800

    memperbaiki link pada medsos

--- partials/heroProfile.php
+++ partials/heroProfile.php
@@ -3,16 +3,17 @@ function heroProfile($bgLink, $profilePicLink, $nama, $quote ,$fb = false, $ig =
     echo'<div class="hero" style="background-image: ' . $bgLink . ';">';
         echo'<div class="bGradient"></div>';
         echo'<div class="container">';
+            // echo'<img src="' . $profilePicLink . '" alt="Profileku">';
             echo'<div class="streetersPicture">';
                 echo'<img src="' . $profilePicLink . '" alt="Profileku">';
                 echo'<div class="storake"></div>';
             echo'</div>';
             echo'<h1>' . $nama . '</h1>';
             echo'<div class="sosmed">';
-                if($fb != false){ echo'<a href="https://' . $fb . '" class="icon fb"></a>'; }
-                if($ig != false){ echo'<a href="https://' . $ig . '" class="icon ig"></a>'; }
-                if($tw != false){ echo'<a href="https://' . $tw . '" class="icon tw"></a>'; }
-                if($web != false){ echo'<a href="https://' . $web . '" class="icon web"></a>'; }
+                if($fb != false){ echo'<a href="' . $fb . '" class="icon fb"></a>'; }
+                if($ig != false){ echo'<a href="' . $ig . '" class="icon ig"></a>'; }
+                if($tw != false){ echo'<a href="' . $tw . '" class="icon tw"></a>'; }
+                if($web != false){ echo'<a href="' . $web . '" class="icon web"></a>'; }
             echo'</div>';
             // echo'<p class="quote">"Fotografi menyadarkanku bahwa setiap detik itu berarti dan setiap hasil yang bagus ada kerja keras"</p>';
             echo'<p class="quote">"' . $quote . '"</p>';
